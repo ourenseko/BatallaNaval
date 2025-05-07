026950cc82a38ab4f4aa941fc6a10ec23a7bf335
Lorena <manuelgarciabaceiredo@gmail.com>
2025-05-07 16:19:22 +0200

    Update play.bat

--- undirlaflota/dist/play.bat
+++ undirlaflota/dist/play.bat
@@ -1,5 +1,9 @@
 @echo off
-title Hundir la flota
+title Hundir la flota v.0.0.1
+echo. -------------------------
+echo.     HUNDIR LA FLOTA
+echo. ------------------------
+echo.
 :game
 java -jar "undirlaflota.jar"
 goto game
